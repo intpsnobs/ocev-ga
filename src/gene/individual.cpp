05d32499403efce3d4f31b567a2f6b8293873d94
RicardoStoklosa <stoklosa.ricardo@gmail.com>
2020-03-09 20:39:59 -0300

    Feat: individual random generation

--- src/gene/individual.cpp
+++ src/gene/individual.cpp
@@ -1,20 +1,70 @@
+#include <bits/stdc++.h>
+
 template <typename T>
 class Individual {
+    static_assert(std::is_same<T, int>::value 
+                || std::is_same<T, double>::value, 
+                "nao pode >:(");
+
 private:
-    T * chromo;
+    T * chromosome;
     int size;
-    static T lowerBound;
-    static T upperBound;
+    T lowerBound;
+    T upperBound;
+    bool isPemutation;
 public:
-    Individual(int chromoSize, T lowerBound = 0, T upperBound = 1);
-}
+    Individual(int chromoSize, T lowerBound = 0, T upperBound = 1, bool isPemutation = false);
+    void generate();
+    T* getChromosome(){
+        return this->chromosome;
+    }
+    int getSize(){
+        return this->size;
+    }
+    T getUpperBound(){
+        return this->upperBound;
+    }
+    friend std::ostream& operator<<(std::ostream& out, const Individual& ind){
+        out<<"[";
+        for(int i=0; i<ind.size; i++){
+            out<<ind.chromosome[i];
+            if(i!=ind.size-1) out<<",";
+        }
+        out<<"]";
+        return out;
+    }
+};                                                                  
 
-template<typename T>
-Individual<T>::Individual(int chromoSize, T lowerBound, T upperBound) {
-    this->chromo = new T[chromoSize];
+template <typename T>
+Individual<T>::Individual(int chromoSize, T lowerBound , T upperBound, bool isPermutation) {
+    this->chromosome = new T[chromoSize];
     this->size = chromoSize;
-    Individual::lowerBound = lowerBound;
-    Individual::upperBound = upperBound;
-
-    
+    this->lowerBound = lowerBound;
+    this->upperBound = upperBound;
+    this->isPemutation = isPemutation;
+    this->generate();
 }
+
+template <> 
+void Individual<int>::generate(){
+    std::random_device rand_dev;
+    std::mt19937 generator(rand_dev());
+    std::uniform_int_distribution<> distr(this->lowerBound, this->upperBound);
+    for(int i=0; i<this->size; i++){
+        this->chromosome[i] = distr(generator);
+    }
+}
+
+template <> 
+void Individual<double>::generate(){
+    std::random_device rand_dev;
+    std::mt19937 generator(rand_dev());
+    std::uniform_real_distribution<> distr(this->lowerBound, this->upperBound);
+    for(int i=0; i<this->size; i++){
+        this->chromosome[i] = distr(generator);;
+    }
+}
+
+// template<class T> inline std::ostream& operator<<(std::ostream& out, const Individual<T>& individual){
+//     return out<<individual->getSize();
+// }
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+#include "gene/individual.cpp"
 
 using namespace std;
 
@@ -8,6 +9,11 @@ int main(int argc, char * argv[]) {
         cerr << "Usage: " << argv[0] << " config_file\n";
         exit(0);
     }
-    
+    Individual<int> intRand(10,0,10,false);
+    cout<<intRand<<endl;
+    Individual<int> binary(10);
+    cout<<binary<<endl;
+    Individual<double> doubleRand(10,0,10,false);
+    cout<<doubleRand<<endl;
     return 0;
 }
