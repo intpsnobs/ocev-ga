aa603e0318a2d318deebe09428971077f0e99b00
RicardoStoklosa <stoklosa.ricardo@gmail.com>
2020-03-11 13:23:54 -0300

    Feat: integer permuted codification

--- src/gene/individual.cpp
+++ src/gene/individual.cpp
@@ -2,8 +2,8 @@
 
 template <typename T>
 class Individual {
-    static_assert(std::is_same<T, int>::value 
-                || std::is_same<T, double>::value, 
+    static_assert(std::is_same<T, int>::value
+                || std::is_same<T, double>::value,
                 "nao pode >:(");
 
 private:
@@ -12,6 +12,7 @@ private:
     T lowerBound;
     T upperBound;
     bool isPemutation;
+    std::vector<int> permutationSequence;
 public:
     Individual(int chromoSize, T lowerBound = 0, T upperBound = 1, bool isPemutation = false);
     void generate();
@@ -20,6 +21,12 @@ public:
     }
     int getSize(){
         return this->size;
+    }
+    bool isPermutation(){
+        return this->isPemutation;
+    }
+    std::vector<int> getPermutationSequence(){
+        return this->permutationSequence;
     }
     T getUpperBound(){
         return this->upperBound;
@@ -34,7 +41,7 @@ public:
         out << "]";
         return out;
     }
-};                                                                  
+};
 
 template <typename T>
 Individual<T>::Individual(int chromoSize, T lowerBound , T upperBound, bool isPermutation) {
@@ -43,20 +50,31 @@ Individual<T>::Individual(int chromoSize, T lowerBound , T upperBound, bool isPe
     this->lowerBound = lowerBound;
     this->upperBound = upperBound;
     this->isPemutation = isPemutation;
+    if(this->isPemutation){
+        for(int i=this->lowerBound; i<=this->upperBound; i++){
+            this->permutationSequence.push_back(i);
+        }
+    }
     this->generate();
 }
 
-template <> 
+template <>
 void Individual<int>::generate(){
     std::random_device rand_dev;
     std::mt19937 generator(rand_dev());
-    std::uniform_int_distribution<> distr(this->lowerBound, this->upperBound);
-    for(int i=0; i<this->size; i++){
-        this->chromosome[i] = distr(generator);
+    if(this->isPemutation){
+        std::vector<int> sequence= this->getPermutationSequence();
+        std::random_shuffle(sequence.begin(),sequence.end());
+        std::copy(sequence.begin(), sequence.end(), this->chromosome);
+    }else{
+        std::uniform_int_distribution<> distr(this->lowerBound, this->upperBound);
+        for(int i=0; i<this->size; i++){
+            this->chromosome[i] = distr(generator);
+        }
     }
 }
 
-template <> 
+template <>
 void Individual<double>::generate(){
     std::random_device rand_dev;
     std::mt19937 generator(rand_dev());
